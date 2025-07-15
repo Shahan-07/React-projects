38ff49583f59a7490f11ab9655e6cc4c9eeb8160
Shahan-07 <iamshahanalam22@gmail.com>
2025-07-15 21:34:52 +0500

    Star - Rating - project

---
+++ star-rating-project/src/App.jsx
@@ -0,0 +1,14 @@
+import react from 'react'
+import './App.css'
+import Rating from './components/Rating'
+
+function App() {
+
+  return (
+    <>
+      <Rating />
+    </>
+  )
+}
+
+export default App
