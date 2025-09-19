e57cf4fcbe23ee724f30a201eafb1675f769a732
Shahan-07 <iamshahanalam22@gmail.com>
2025-09-20 00:16:20 +0500

    Search filter in list project

---
+++ search-filter/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Search from './component/Search'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='App'>
+        <Search />
+      </div>
+    </>
+  )
+}
+
+export default App
